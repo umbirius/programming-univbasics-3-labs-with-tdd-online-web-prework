53b2814bcb9d9787f39551d7e9671ce9f9213656
loading-actor-9896 <upalazzo93@gmail.com>
2019-08-05 23:27:12 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,6 @@
-# Add your variables here
+int first_number = 5 # Add your variables here
+int second_number = 2
+int sum = (first_number)+(second_number)
+int difference = (first_number)-(second_number)
+int product = (first_number)*(second_number)
+int quotient = (first_number)/(second_number)
