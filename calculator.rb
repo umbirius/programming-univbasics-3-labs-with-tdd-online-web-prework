3f9c87a6f01fa18407ff39f592089db284a30d2e
loading-actor-9896 <upalazzo93@gmail.com>
2019-08-05 23:29:55 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,8 +1,18 @@
-int first_number = 5 # Add your variables here
-int second_number = 2
-sum (first_number, second_number)
+first_number = 5 # Add your variables here
+second_number = 2
+
+def sum (first_number, second_number)
     first_number + second_number 
   end 
-int difference = (first_number)-(second_number)
-int product = (first_number)*(second_number)
-int quotient = (first_number)/(second_number)
+  
+def difference (first_number, second_number)
+    first_number - second_number 
+  end 
+  
+def product (first_number, second_number)
+    first_number * second_number 
+  end 
+  
+def quotient (first_number, second_number)
+    first_number * second_number 
+  end 
